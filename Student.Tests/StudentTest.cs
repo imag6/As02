5c8156b8f09cf1aff04f23edb9568d556f8c9235
Camille <cgon@itu.dk>
2021-09-18 11:03:03 +0200

    Added code to Student and test

---
+++ Student.Tests/StudentTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace Student.Tests
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void printingOutCorrectString()
+        {
+            //var student = new Student(123456, "Sanne", "Hansen", "Active", new DateTime(2020, 08, 30), new DateTime(2023, 06, 30), new DateTime(2023, 06, 30));
+            var student = new Student
+            {
+                Id = 12345,
+                GivenName = "Sanne",
+                Surname = "Hansen",
+                status = Status.Active,
+                StartDate = new DateTime(2020, 08, 30),
+                EndDate = new DateTime(2023, 06, 30),
+                Graduation = new DateTime(2023, 06, 30)
+            };
+                
+            var output = student.ToString(); 
+
+            var expected = "Sanne Hansen (123456): Active [Enrolled: 30.08.2020, Enddate: 30.06.23], Expected Graduation: 15.06.23";
+           
+            Assert.Equal(expected, output);
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Student
+{
+    public class Student
+    {
+        int Id;
+        string GivenName;
+        string Surname;
+        readonly Status status;
+        
+        DateTime StartDate; //DD.MM.YY
+        DateTime EndDate; 
+        DateTime Graduation;
+
+        public override string ToString()
+        {
+            return GivenName + " " + Surname + " (" + id + "): " + status + " [Enrolled: " + StartDate + ", Enddate: " + EndDate + ", Expected Graduation: " + Graduation + "]";
+        }
+
+         public int id
+        {
+            get; init;
+        }
+        
+        public enum Status
+        {
+            New, 
+            Active, 
+            Droupout, 
+            Gratuated
+        }
+    }
+}
