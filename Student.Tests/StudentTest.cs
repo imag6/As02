a15de2055a249aaa1143d046f6269943788d5553
frev <frev@itu.dk>
2021-09-20 15:15:24 +0200

    Datetime formatting

--- Student.Tests/StudentTest.cs
+++ Student.Tests/StudentTest.cs
@@ -16,16 +16,18 @@ namespace Student.Tests
                 GivenName = "Sanne",
                 Surname = "Hansen",
                 status = Status.Active,
-                StartDate = new DateTime(2020, 08, 30), //datetime formatting
+                StartDate = new DateTime(2020, 08, 30), 
                 EndDate = new DateTime(2023, 06, 30),
-                Graduation = new DateTime(2023, 06, 30)
+                Graduation = new DateTime(2023, 06, 15)
             };
                 
             var output = student.ToString(); 
 
-            var expected = "Sanne Hansen (123456): Active [Enrolled: 30.08.2020, Enddate: 30.06.23], Expected Graduation: 15.06.23";
+            var expected = "Sanne Hansen (123456): Active [Enrolled: 30.08.2020, Enddate: 30.06.2023, Expected Graduation: 15.06.2023]";
            
             Assert.Equal(expected, output);
         }
+
+        //do test where we create two separate objects with the same fields, and compare.
     }
 }
